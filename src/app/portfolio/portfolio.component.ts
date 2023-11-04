398f44a08fb589b7ff436e7a60fab6850e5ad6bf
AnneEberhard <anne.eberhard@gmx.net>
2023-11-04 11:32:06 +0100

    added shadows and designs

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class PortfolioComponent {
 
+  portfolio = [{
+    image:'assets/img/Pollo loco.png',
+    header: 'Join',
+    subheader: 'Javascript | HTML | CSS',
+    text:'Task manager based on the Kanban system. Allows for creating tasks, assigning users, subtasks and categories as well as organizing them into different areas',
+    linkLive:'http://anne-eberhard.developerakademie.net/Join-Mono/index.html',
+    linkGit:'https://github.com/AnneEberhard/Join-Mono'
+  },
+  {
+    image:'assets/img/Pollo loco.png',
+    header: 'El Pollo Loco',
+    subheader: 'Javascript | HTML | CSS',
+    text:'A Jump-and-Run game built in an object-oriented design. Help Pepe gather coins and salsa bottles to defeat the enemy chicks.',
+    linkLive:'http://anne-eberhard.developerakademie.net/ElPolloLoco/index.html',
+    linkGit:'https://github.com/AnneEberhard/ElPolloLoco'
+  }
+,{
+  image:'assets/img/Pollo loco.png',
+  header: 'Simple CRM',
+  subheader: 'Angular | Firebase | Javascript | HTML | CSS',
+  text:'tbd.',
+  linkLive:'',
+  linkGit:''
+},
+{
+  image:'assets/img/Pollo loco.png',
+  header: 'Bookstagram',
+  subheader: 'Javascript | HTML | CSS',
+  text:'A bookstagram page based on Instagram.',
+  linkLive:'http://anne-eberhard.developerakademie.net/instagram/index.html',
+  linkGit:'https://github.com/AnneEberhard/myInstagram'
+}]
+
 }
