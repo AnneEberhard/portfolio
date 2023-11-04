41e378774e5c361fc7066fa40dd7e1047de748fe
AnneEberhard <anne.eberhard@gmx.net>
2023-11-04 08:26:38 +0100

    added JSON for skills instead of hard HTML

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -7,6 +7,58 @@ import { PageService } from '../page.service';
   styleUrls: ['./my-skills.component.scss']
 })
 export class MySkillsComponent {
+ skillSet = [{
+  icon : 'assets/img/icons/angular_big.png',
+  alt: 'angular logo',
+  title: 'Angular',
+ },
+ {
+  icon : 'assets/img/icons/css_big.png',
+  alt: 'css logo',
+  title: 'CSS',
+ },
+ {
+  icon : 'assets/img/icons/firebase_big.png',
+  alt: 'firebase logo',
+  title: 'Firebase',
+ },
+ {
+  icon : 'assets/img/icons/git_big.png',
+  alt: 'git  logo',
+  title: 'GIT',
+ },
+ {
+  icon : 'assets/img/icons/html_big.png',
+  alt: 'html logo',
+  title: 'HTML',
+ },
+ {
+  icon : 'assets/img/icons/js_big.png',
+  alt: 'javascript logo',
+  title: 'JavaScript',
+ },
+ {
+  icon : 'assets/img/icons/matdes_big.png',
+  alt: 'material design logo',
+  title: 'Material design',
+ },
+ {
+  icon : 'assets/img/icons/api_big.png',
+  alt: 'rest-api logo',
+  title: 'REST-API',
+ },
+ {
+  icon : 'assets/img/icons/scrum_big.png',
+  alt: 'scrum logo',
+  title: 'Scrum',
+ },
+ {
+  icon : 'assets/img/icons/ts_big.png',
+  alt: 'typescript logo',
+  title: 'TypeScript',
+ },
+]
+
   constructor(private pageService: PageService) {}
 
   scrollToSection(sectionId: string) {
