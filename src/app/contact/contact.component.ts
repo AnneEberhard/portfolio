52370e70413a9ff14ceca9434a2f998df02b8f7d
AnneEberhard <anne.eberhard@gmx.net>
2023-11-04 16:29:29 +0100

    contact form basic

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+ @ViewChild('contactForm') contactForm! : any; //ElementRef
+ @ViewChild('nameField') nameField! : any; //ElementRef
+ @ViewChild('emailField') emailField! : any; //ElementRef
+ @ViewChild('messageField') messageField! : any; //ElementRef
+ @ViewChild('sendButton') sendButton! : any; //ElementRef
+
+   
+ async sendMail(){
+  console.log('begin')
+  let nameField = this.nameField.nativeElement; 
+  let emailField = this.emailField.nativeElement;
+  let messageField =  this.messageField.nativeElement;
+  let sendButton = this.sendButton.nativeElement;
+    nameField.disabled = true;
+    emailField.disabled = true;
+    messageField.disabled = true;
+    sendButton.disabled = true;
+// Animation anzeigen
+    const data = new FormData();
+    data.append('name',nameField.value);
+    data.append('email',emailField.value);
+    data.append('message', messageField.value);
+
+    await fetch("https://formspree.io/f/xwkdzbgo", {
+        method: "POST",
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        //text anzeigen für Nachricht gesendet
+        console.log('send', this.contactForm)
+        nameField.disabled = false;
+        emailField.disabled = false;
+        messageField.disabled = false;
+        sendButton.disabled = false;
+    }).catch((error) => {
+        console.log(error);
+    });
+}
 
 }
